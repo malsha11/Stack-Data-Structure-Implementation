cf20cbed275ff110431721425b0ca26b202d8b67
malsha11 <96194@fhss.sjp.ac.lk>
2021-06-18 02:29:27 +0530

    Modified pop Method caller

--- Question 01/main.py
+++ Question 01/main.py
@@ -11,7 +11,8 @@ myStack.push(30)
 myStack.push(40)
 myStack.push(50)
 myStack.push(60)
-print("Stack Pop Element is :" , myStack.pop())
+print("Stack Pop Element is :" ,myStack.pop())# Remove the last element in stack
+
 print("Stack Top element is :" , myStack.top())
 print("Stack size :" , myStack.size())
 print("Is Stack Full ? : " , myStack.isFullStack())
